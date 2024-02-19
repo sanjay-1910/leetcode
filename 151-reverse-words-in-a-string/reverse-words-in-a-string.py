c874dd540ee1b02faf66d5a19eb76e8d7f47b182
sanjay-1910 <144416808+sanjay-1910@users.noreply.github.com>
2024-02-19 17:16:12 +0530

    Time: 33 ms (82.42%) | Memory: 16.7 MB (90.00%) - LeetSync

--- 151-reverse-words-in-a-string/reverse-words-in-a-string.py
+++ 151-reverse-words-in-a-string/reverse-words-in-a-string.py
@@ -1,20 +1,22 @@
 class Solution:
     def reverseWords(self, s: str) -> str:
-        sa=""
-        fin=""
-        for i in range(len(s)-1,-1,-1):
-            if s[i]==" " and len(fin)==0:
-                fin=fin+sa
-                sa=""
-            elif s[i]==" " and len(sa)!=0:
-                fin=fin+" "+sa
-                sa=""
-            elif s[i]==" " and len(sa)==0:
-                pass
-            else:
-                sa=s[i]+sa
-        if s[0]==" ":
-            return fin.strip()
-        else:
-            fin=fin+" "+sa
-            return fin.strip()
+        # sa=""
+        # fin=""
+        # for i in range(len(s)-1,-1,-1):
+        #     if s[i]==" " and len(fin)==0:
+        #         fin=fin+sa
+        #         sa=""
+        #     elif s[i]==" " and len(sa)!=0:
+        #         fin=fin+" "+sa
+        #         sa=""
+        #     elif s[i]==" " and len(sa)==0:
+        #         pass
+        #     else:
+        #         sa=s[i]+sa
+        # if s[0]==" ":
+        #     return fin.strip()
+        # else:
+        #     fin=fin+" "+sa
+        #     return fin.strip()
+        s=s.split()
+        return " ".join(s[::-1])
