2a16dd4311c84c14745549a18d96098889a60346
sanjay-1910 <144416808+sanjay-1910@users.noreply.github.com>
2024-02-19 17:59:49 +0530

    Time: 88 ms (87.00%) | Memory: 61.6 MB (62.25%) - LeetSync

---
+++ 1798-max-number-of-k-sum-pairs/max-number-of-k-sum-pairs.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int maxOperations(vector<int>& nums, int k) {
+        sort(nums.begin(),nums.end());
+        int i=0;
+        int c=0;
+        int j=nums.size()-1;
+        while(i<=j && nums.size()>0)
+        {
+            if(nums[i]+nums[j]==k && i!=j)
+            {
+                c=c+1;
+                // nums.erase(nums.begin()+i);
+                // nums.erase(nums.begin()+j);
+                i=i+1;
+                j=j-1;
+            }
+            else if(nums[i]+nums[j]>k)
+            {
+                j=j-1;
+            }
+            else
+            {
+                i=i+1;
+            }
+        }
+        return c;   
+    }
+};
