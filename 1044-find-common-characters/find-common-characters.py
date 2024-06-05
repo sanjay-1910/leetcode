b04686e28db514546f1d9725d77100acf1c2cfcb
sanjay-1910 <144416808+sanjay-1910@users.noreply.github.com>
2024-06-05 19:21:26 +0530

    Time: 51 ms (48.74%) | Memory: 16.6 MB (93.72%) - LeetSync

--- 1044-find-common-characters/find-common-characters.py
+++ 1044-find-common-characters/find-common-characters.py
@@ -1,28 +1,25 @@
 class Solution:
     def commonChars(self, words: List[str]) -> List[str]:
-        d = {}
-        for char in words[0]:
-            d[char] = d.get(char, 0) + 1
-
-        # Intersect with counts from subsequent words
-        for word in words[1:]:
-            e = {}
-            for char in word:
-                if char in d:
-                    e[char] = e.get(char, 0) + 1
-            # Update d to keep the minimum counts
-            for char in d:
-                if char in e:
-                    d[char] = min(d[char], e[char])
-                else:
-                    d[char] = 0
-
-        # Build the result list from the count dictionary
-        result = []
-        for char, count in d.items():
-            result.extend([char] * count)
-        
-        return result
+        d={}
+        l=[]
+        for i in words[0]:
+            if i not in d.keys():
+                d[i]=1
+            else:
+                d[i]+=1
+        for i in range(1,len(words)):
+            e={}
+            for j in words[i]:
+                if j in d.keys():
+                    if j in e.keys() and e[j]<d[j]:
+                        e[j]+=1
+                    if j not in e.keys():
+                        e[j]=1
+            d=e
+        for i in d.keys():
+            for j in range(0,d[i]):
+                l.append(i)
+        return l
             
 
 
