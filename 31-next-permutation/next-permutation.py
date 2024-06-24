cde603ba24e62101c55fd123c8126597760861b4
sanjay-1910 <144416808+sanjay-1910@users.noreply.github.com>
2024-06-24 22:00:29 +0530

    Time: 22 ms (63.89%) | Memory: 11.5 MB (92.05%) - LeetSync

---
+++ 31-next-permutation/next-permutation.py
@@ -0,0 +1,26 @@
+class Solution(object):
+    def nextPermutation(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: None Do not return anything, modify nums in-place instead.
+        """
+        n=len(nums)
+        brea=-1
+        for i in range(n-2,-1,-1):
+            if nums[i]<nums[i+1]:
+                brea=i
+                break
+        if brea==-1:
+            nums.reverse()
+            return nums
+        for i in range(n-1,brea,-1):
+            if nums[i]>nums[brea]:
+                temp=nums[i]
+                nums[i]=nums[brea]
+                nums[brea]=temp
+                break
+        nums[brea+1: ]=reversed(nums[brea+1:])
+        return nums
+                
+                
+        
