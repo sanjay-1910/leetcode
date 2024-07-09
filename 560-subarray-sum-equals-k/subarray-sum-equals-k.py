b0287781ae40b07ec9b629932d3411dd41a2e585
sanjay-1910 <144416808+sanjay-1910@users.noreply.github.com>
2024-07-10 00:39:30 +0530

    Time: 202 ms (98.85%) | Memory: 14.2 MB (65.83%) - LeetSync

--- 560-subarray-sum-equals-k/subarray-sum-equals-k.py
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.py
@@ -5,44 +5,14 @@ class Solution(object):
         :type k: int
         :rtype: int
         """
-        # def prefix(t,i):
-        #     for j in range(0,i):
-        #         if t in d.keys():
-        #             return d[t]
-        #     else:
-        #         return -1
-        # sumo=0
-        # count=0
-        # d={}
-        # for i in range(0,len(nums)):
-        #     sumo=sumo+nums[i]
-        #     d[sumo]=i
-        #     if sumo==k:
-        #         count+=1
-        #     elif sumo>k:
-        #         t=(sumo-k)
-        #         if prefix(t,i)>=0:
-        #             count+=1
-        #     elif k==0:
-        #         if nums[i]==0:
-        #             count+=1
-        #         elif nums[i]<0:
-        #             t=sumo
-        #             if prefix(t,i)>=0:
-        #                 count+=1
-        # return count
         sumo = 0
         count = 0
-        d = {0: 1}  # Initialize the dictionary with sum 0 having one count
+        d = {0:1}  # Initialize the dictionary with sum 0 having one count
 
         for i in range(len(nums)):
             sumo += nums[i]
-
-            # Check if (sumo - k) is in the dictionary
             if sumo - k in d:
                 count += d[sumo - k]
-
-            # Add sumo to the dictionary
             if sumo in d:
                 d[sumo] += 1
             else:
