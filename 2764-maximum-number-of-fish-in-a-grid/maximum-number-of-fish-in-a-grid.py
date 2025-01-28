b43b13c3716f1365b6f3750413ff6b3f8dcd147f
sanjay-1910 <144416808+sanjay-1910@users.noreply.github.com>
2025-01-28 19:04:00 +0530

    Time: 80 ms (9.27%) | Memory: 17.9 MB (49.02%) - LeetSync

---
+++ 2764-maximum-number-of-fish-in-a-grid/maximum-number-of-fish-in-a-grid.py
@@ -0,0 +1,70 @@
+class Solution:
+    def findMaxFish(self, grid: List[List[int]]) -> int:
+        def dfs(i,j,grid,dp):
+            if(dp[i][j]!=-1):
+                return dp[i][j]
+            queue = [(i,j)]
+            visited = set()
+            visited.add((i,j))
+            cost = grid[i][j]
+            grid[i][j] = 0
+            # directions = [[i-1,j],[i+1,j],[i,j-1],[i,j+1]]
+            while(len(queue)!=0):
+                ni,nj = queue.pop(0)
+                directions = [[ni-1,nj],[ni+1,nj],[ni,nj-1],[ni,nj+1]]
+                for dir in directions:
+                    ni,nj = dir
+                    if(ni<0 or nj<0 or ni>=len(grid) or nj==len(grid[0]) or grid[ni][nj]==0):
+                        continue
+                    else:
+                        if((ni,nj) not in visited):
+                            cost+=dfs(ni,nj,grid,dp)
+                            visited.add((ni,nj))
+                            queue.append((ni,nj))
+            dp[i][j] = cost
+            return cost
+        dp = [[-1]*len(grid[0]) for i in range(len(grid))]
+        final = 0
+        for i in range(0,len(grid)):
+            ans = 0
+            for j in range(0,len(grid[i])):
+                if(grid[i][j] != 0):
+                    ans = dfs(i,j,grid,dp)
+                final = max(ans,final)
+        return final
+
+
+
+#another approach
+
+# class Solution:
+#     def findMaxFish(self, grid: List[List[int]]) -> int:
+#         def dfs(i, j):
+#             if i < 0 or j < 0 or i >= len(grid) or j >= len(grid[0]) or grid[i][j] == 0:
+#                 return 0
+
+#             # Capture the current value and mark the cell as visited
+#             fish = grid[i][j]
+#             grid[i][j] = 0  # Mark as visited by setting to 0
+
+#             # Explore all 4 directions
+#             fish += dfs(i - 1, j)
+#             fish += dfs(i + 1, j)
+#             fish += dfs(i, j - 1)
+#             fish += dfs(i, j + 1)
+
+#             return fish
+
+#         max_fish = 0
+#         for i in range(len(grid)):
+#             for j in range(len(grid[0])):
+#                 if grid[i][j] != 0:
+#                     max_fish = max(max_fish, dfs(i, j))
+
+#         return max_fish
+
+                
+                
+
+        
+        
