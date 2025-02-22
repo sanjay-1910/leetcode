1ec4038fcbcee1da0d2ad3a97af24b86199b70c3
sanjay-1910 <144416808+sanjay-1910@users.noreply.github.com>
2025-02-22 23:04:18 +0530

    Time: 0 ms (100%), Space: 10.3 MB (73.77%) - LeetHub

---
+++ 0032-longest-valid-parentheses/0032-longest-valid-parentheses.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int longestValidParentheses(string s) {
+        stack<int> st;  // Use a stack to store indices only (char is unnecessary)
+        int last_popped = -1;  // Stores the last valid index that was popped
+        int answer = 0; 
+        
+        for (int i = 0; i < s.size(); i++) {
+            if (s[i] == '(') {
+                st.push(i);
+            } else {  // s[i] == ')'
+                if (!st.empty()) {
+                    st.pop();  // Match found, pop the stack
+                    
+                    if (!st.empty()) {
+                        // If stack is not empty, valid substring is from st.top() to i
+                        answer = max(answer, i - st.top());
+                    } else {
+                        // If stack is empty, valid substring is from last_popped to i
+                        answer = max(answer, i - last_popped);
+                    }
+                } else {
+                    // No matching '(' found, update last_popped
+                    last_popped = i;
+                }
+            }
+        }
+        return answer;
+    }
+};
