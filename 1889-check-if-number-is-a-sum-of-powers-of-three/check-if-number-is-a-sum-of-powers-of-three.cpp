dbe1b4308bb212e680c6e749637f96532cf69469
sanjay-1910 <144416808+sanjay-1910@users.noreply.github.com>
2025-03-04 13:32:57 +0530

    Time: 31 ms (8.45%) | Memory: 7.8 MB (67.84%) - LeetSync

--- 1889-check-if-number-is-a-sum-of-powers-of-three/check-if-number-is-a-sum-of-powers-of-three.cpp
+++ 1889-check-if-number-is-a-sum-of-powers-of-three/check-if-number-is-a-sum-of-powers-of-three.cpp
@@ -1,31 +1,28 @@
-#include <cmath>
+#include <cmath> 
 
 class Solution {
 public:
     bool check(int n, int power, int sum) {
         int value = pow(3, power);
         
-        if (sum == n) {  // Corrected base case
-            return true;
-        }
-        if (value > n || sum > n) {  
+        if (value > n || sum+value > n) {
             return false;
         }
-
-        // Take the current power of 3
-        if (check(n, power + 1, sum + value)) {
+        if (value == n || sum+value == n) {
             return true;
         }
-        
-        // Skip the current power of 3
+
+        if (check(n, power + 1, sum+value)) {
+            return true;
+        }
         if (check(n, power + 1, sum)) {
             return true;
         }
-
+        
         return false;
     }
 
     bool checkPowersOfThree(int n) {
-        return check(n, 0, 0);
+        return check(n, 0, 0);    
     }
 };
