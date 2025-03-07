def091bf3c4cab5ac65a83023c8b4e58b66a17bf
sanjay-1910 <144416808+sanjay-1910@users.noreply.github.com>
2025-03-07 20:09:22 +0530

    Time: 113 ms (40.30%) | Memory: 55.6 MB (8.46%) - LeetSync

---
+++ 1073-number-of-enclaves/number-of-enclaves.py
@@ -0,0 +1,32 @@
+class Solution:
+    def dfs(self,i,j,grid,new,visited):
+        new[i][j] = 0
+        visited[i][j] = True
+        directions = [(i,j+1),(i+1,j),(i,j-1),(i-1,j)]
+        for dir in directions:
+            row = dir[0]
+            col = dir[1]
+            if(row >=0 and col<len(grid[0]) and col>=0 and row< len(grid) and visited[row][col] == False and grid[row][col] == 1):
+                self.dfs(row,col,grid,new,visited)
+    def numEnclaves(self, grid: List[List[int]]) -> int:
+        visited = [[False]*len(grid[0]) for d in range(len(grid))]
+        # new =  [[]*len(grid[0]) for d in range(len(grid))]
+        new = grid
+        c = 0
+        for i in range(0,len(grid[0])):
+            if(grid[0][i] == 1 and not visited[0][i]):
+                self.dfs(0,i,grid,new,visited)
+            if(grid[len(grid)-1][i] == 1 and not visited[len(grid)-1][i]):
+                self.dfs(len(grid)-1,i,grid,new,visited)
+        for j in range(0,len(grid)):
+            if(grid[j][0] == 1 and not visited[j][0]):
+                self.dfs(j,0,grid,new,visited)
+            if(grid[j][len(grid[0])-1] == 1 and not visited[j][len(grid[0])-1]):
+                self.dfs(j,len(grid[0])-1,grid,new,visited)
+        for k in range(0,len(new)):
+            for l in range(0,len(new[0])):
+                if(new[k][l] == 1):
+                    c+=1
+        return c
+
+        
