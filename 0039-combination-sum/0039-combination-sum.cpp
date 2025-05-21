07a640c93f8689a96ee08a933e8855ca200c6289
sanjay-1910 <144416808+sanjay-1910@users.noreply.github.com>
2025-05-21 09:57:25 +0530

    Time: 74 ms (11.84%), Space: 69.6 MB (9.61%) - LeetHub

---
+++ 0039-combination-sum/0039-combination-sum.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    vector<vector<int>>final;
+    void san(vector<int>&candidates,vector<int>ans,int sum,int target,int index,int size)
+    {
+        if(sum>target || index>=size)
+        {
+            return;
+        }
+        if(sum==target)
+        {
+            final.push_back(ans);
+            return;
+        }
+        ans.push_back(candidates[index]);
+        san(candidates,ans,sum+candidates[index],target,index,size);
+        ans.pop_back();
+        san(candidates,ans,sum,target,index+1,size);
+
+    }
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        vector<int>ans;
+        san(candidates,ans,0,target,0,candidates.size());
+        return final;
+        
+    }
+};
