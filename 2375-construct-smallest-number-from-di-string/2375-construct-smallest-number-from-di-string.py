51b1f8094d4ec4440ad3569471698e3914c38955
sanjay-1910 <144416808+sanjay-1910@users.noreply.github.com>
2025-08-16 15:04:06 +0530

    Time: 0 ms (100%), Space: 17.9 MB (19.48%) - LeetHub

--- 2375-construct-smallest-number-from-di-string/2375-construct-smallest-number-from-di-string.py
+++ 2375-construct-smallest-number-from-di-string/2375-construct-smallest-number-from-di-string.py
@@ -1,61 +1,18 @@
 class Solution:
-    def small(self,pattern,ans,used,prev_num,index):
-        if(index == len(pattern)+1):
-            return True
-        start = 0
-        dec = 0
-        end = 0
-        if(pattern[index-1] == 'I'):
-            prev_num += 1
-            dec = 1
-            end = len(pattern)+2
-        else:
-            prev_num -= 1
-            dec = -1
-            end = 0
-        while(prev_num!=end):
-            if(used[prev_num]!=False):
-                prev_num += dec
-                continue
-            if(used[prev_num] == False):
-                ans.append(prev_num)
-                used[prev_num] = True
-                if(self.small(pattern,ans,used,prev_num,index+1)):
-                    return True
-                ans.pop()
-                used[prev_num] = False
-            prev_num += dec
-        return False
     def smallestNumber(self, pattern: str) -> str:
         ans = []
-        used = {}
-        for i in range(1,len(pattern)+2):
-            used[i] = False
-        for number in range(1,len(pattern)+2):
-            ans = [number]
-            used[number] = True
-            if(self.small(pattern,ans,used,number,1)):
-                result = ''.join(map(str,ans))
-                return result
-            ans.pop()
-            used[number] = False
-        return False
+        stack = []
+
+        for i in range(len(pattern) + 1):
+            stack.append(str(i + 1))   # push the next number
+            if i == len(pattern) or pattern[i] == "I":
+                while stack:
+                    ans.append(stack.pop())
+
+        return "".join(ans)
+
+                
 
 
-
-
-          # else:
-        #     dec = 0
-        #     end = 0
-        #     if(pattern[index-1] == 'I'):
-        #         dec = 1
-        #         end = len(pattern)
-        #     else:
-        #         dec = -1
-        #         end = 1
-        #     while(number!=end):
-        #         if(used[number] == False):
-        #             ans.append(number)
-        #             used[number] = True
                   
         
