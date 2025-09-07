935b5e4d939a1aa44dfc22452874e013b089fb0f
sanjay-1910 <144416808+sanjay-1910@users.noreply.github.com>
2025-09-07 22:42:39 +0530

    Time: 349 ms (76.45%), Space: 0B (100%) - LeetHub

---
+++ 1050-actors-and-directors-who-cooperated-at-least-three-times/1050-actors-and-directors-who-cooperated-at-least-three-times.sql
@@ -0,0 +1,7 @@
+# Write your MySQL query statement below
+-- select actor_id, director_id from ActorDirector group by actor_id,director_id having count(*)>3;
+
+SELECT actor_id, director_id
+FROM ActorDirector
+GROUP BY actor_id, director_id
+HAVING COUNT(*) >= 3;
