bf099594d78f9bb3e1ba42875ebae947793793be
sanjay-1910 <144416808+sanjay-1910@users.noreply.github.com>
2025-10-10 18:47:03 +0530

    Time: 291 ms (5.31%) | Memory: 18.3 MB (19.66%) - LeetSync

--- 1777-determine-if-two-strings-are-close/determine-if-two-strings-are-close.py
+++ 1777-determine-if-two-strings-are-close/determine-if-two-strings-are-close.py
@@ -4,8 +4,6 @@ class Solution:
             return False
         w1 = {}
         w2 = {}
-        # l1 = []
-        # l2 = []
         for i in range(0,len(word1)):
             if word1[i] not in w1.keys():
                 w1[word1[i]] = 1
@@ -15,13 +13,7 @@ class Solution:
                 w2[word2[i]] = 1
             else:
                 w2[word2[i]] += 1
-        # for key in w1.keys():
-        #     l1.append(w1[key])
-        # for k in w2.keys():
-        #     l2.append(w2[k])
-        # l1.sort()
-        # l2.sort()
-        return sorted(w1.values()) == sorted(w2.values()) and set(w1.keys()) == set(w2.keys())
+        return sorted(w1.values()) == sorted(w2.values()) and w1.keys() == w2.keys()
 
 
 
